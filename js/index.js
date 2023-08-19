615028af0ee0e91dae71f84854e5206c913cc160
Shanzey22 <shanzeyshaikh22@gmail.com>
2023-08-20 03:48:21 +0530

    added brands names

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-confirm("GadgetTradeHub.in wants to start sending you push notifications. Click ok to subscribe.");
+confirm("GadgetTradeHub.in wants to start sending you push notifications. Click OK to Subscribe.");
+
+
+
+function left4Scroll() {
+    const left = document.querySelector(".scroll-images");
+    left.scrollBy(200, 0);
+  }
+  function rightScroll() {
+    const right = document.querySelector(".scroll-images");
+    right.scrollBy(-200, 0);
+  }
