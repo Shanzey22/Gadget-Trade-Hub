db8a6b946dbb302b0df6d481a8b5a05563ebd12b
lokeshmanapure <lokeshmanapure1@gmail.com>
2023-08-23 19:11:09 +0530

    light fixed

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ function toggleDarkTheme(){
     bodyElement.classList.remove(' top-navbar-light');
         bodyElement.classList.add('top-navbar-dark');
 }
+function toggleLightTheme(){
+    const bodyElement=document.getElementById('body');
+    bodyElement.classList.remove('body-dark');
+    bodyElement.classList.add('body-light');
+    bodyElement.classList.remove(' top-navbar-dark');
+        bodyElement.classList.add('top-navbar-light');
+}
